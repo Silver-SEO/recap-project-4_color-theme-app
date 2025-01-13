88946299a1376f072900340e67593c56d09282be
Alexander <mail@alexandersaupe.de>
2025-01-13 16:53:37 +0100

    Add challenge 2 to recap project 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import { initialColors } from "./lib/colors";
+import ColorForm from "./Components/ColorForm/ColorForm.jsx";
 import Color from "./Components/Color/Color";
 import "./App.css";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  function handleSubmit(data) {
+    setColors((prevColors) => [data, ...prevColors]);
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
-
-      {initialColors.map((color) => {
+      <ColorForm onSubmitColor={handleSubmit} />
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
